76af20f4429496f778db382d11caaccebe0ac3e9
vineet-proptiger <vineet.mishra@proptiger.com>
2026-08-05 17:45:57 +0530

    ttt

--- app/sitemap.js
+++ app/sitemap.js
@@ -8,11 +8,11 @@ export default function sitemap() {
       changeFrequency: 'monthly',
       priority: 1,
     },
-    {
-      url: `${BASE_URL}/privacy-policy`,
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 0.8,
-    },
+    // {
+    //   url: `${BASE_URL}/privacy-policy`,
+    //   lastModified: new Date(),
+    //   changeFrequency: 'yearly',
+    //   priority: 0.8,
+    // },
   ]
 }
